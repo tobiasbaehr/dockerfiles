5b117f1b41d68fb6ca3c57d24140825b0e314a13
tobiasb <tobias.baehr@reinblau.de>
2014-06-28 14:40:14 +0200

    [mysql]Fixed start.sh, dockerfile etc.

--- mysql/run.sh
+++ mysql/run.sh
@@ -1,36 +1,21 @@
 #!/usr/bin/env bash
-set -o errexit
+#set -o errexit
 #set -x
-
-TAG="reinblau/mysql"
-NAME="mysql"
 
 __DIR__="$(cd "$(dirname "${0}")"; echo $(pwd))"
 __BASE__="$(basename "${0}")"
 __FILE__="${__DIR__}/${__BASE__}"
 
-build() {
-  if [ "$(docker inspect ${TAG} > /dev/null 2>&1)" ];then
-    docker build -t ${TAG} --rm=true ${__DIR__}
-  fi
-}
-
 start () {
-  docker run -d --publish-all --name=${NAME} -v /data/log/mysql:/var/log/mysql -v /data/lib/mysql:/var/lib/mysql ${TAG}
+  cd "${__DIR__}" && crane lift
 }
 
-restart () {
-  if [ "$(docker inspect ${NAME} > /dev/null 2>&1)" ];then
-    start
-  else
-    docker restart ${NAME}
-  fi
+update () {
+ echo "TODO"
 }
 
 main () {
-  crane lift --manifest="${__DIR__}/crane.yml" 2> /dev/null || true
-  #build
-  #restart
+  start
 }
 
 main
