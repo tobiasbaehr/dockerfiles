3c09fae7d0ac9aceeab92a6fd93f59a123778208
tobiasb <tobias.baehr@reinblau.de>
2014-06-29 17:56:13 +0200

    Prepared basic_lamp for new provisioner.

--- basic_lamp/README.md
+++ basic_lamp/README.md
@@ -3,10 +3,9 @@ RB_Basic_LAMP
 
 Dockerfile for PHP-Projects with MySql database
 
-Uses as base image [rb-basic-web](https://index.docker.io/u/tobiasb/rb-basic-web/)
+Uses as base image [rb-basic-web](https://registry.hub.docker.com/u/tobiasb/rb-basic-web/)
 
 Tools
 ------
 
-* Mysql
-* PHPMyAdmin
+* Mysql client
--- basic_lamp/build_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm ~/.ssh/known_hosts 2>/dev/null
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-#docker rmi tobiasb/rb-basic-lamp
-docker build  -t tobiasb/rb-basic-lamp .
-docker run -d -p 49102:22 -p 8082:80 -v /vagrant_data/build_test:/var/www/ tobiasb/rb-basic-lamp
-sleep 2
-ssh localhost -p 49102 -l dev
