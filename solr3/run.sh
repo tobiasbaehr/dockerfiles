238b317ea63bd28480028431e9957cd1028629ff
tobiasb <tobias.baehr@reinblau.de>
2014-08-13 14:08:40 +0200

    Added new service solr3

---
+++ solr3/run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -o errexit
+#set -x
+
+__DIR__="$(cd "$(dirname "${0}")"; echo $(pwd))"
+__BASE__="$(basename "${0}")"
+__FILE__="${__DIR__}/${__BASE__}"
+
+start () {
+  cd "${__DIR__}" && crane lift
+}
+
+main () {
+  start
+}
+
+main
