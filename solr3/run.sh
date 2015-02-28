7a0c7be72d483aea9cdfe1324a4faaa780d1e948
tobiasb <tobias.baehr@reinblau.de>
2015-03-01 00:21:43 +0100

    [solr3]Removed crane file.

--- solr3/run.sh
+++ solr3/run.sh
@@ -1,13 +1,12 @@
-#!/usr/bin/env bash
-set -o errexit
-#set -x
+#!/bin/bash
 
 __DIR__="$(cd "$(dirname "${0}")"; echo $(pwd))"
 __BASE__="$(basename "${0}")"
 __FILE__="${__DIR__}/${__BASE__}"
 
 start () {
-  cd "${__DIR__}" && crane lift
+  docker ps | grep "reinblau/solr3" >/dev/null || docker start solr3 >/dev/null 2>&1 || \
+  docker run --detach --publish-all --name solr3 reinblau/solr3 >/dev/null
 }
 
 main () {
