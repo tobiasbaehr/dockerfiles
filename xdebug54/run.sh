1425fd0f54203fde1cf10ded985a67816a4a3b22
tobiasb <tobias.baehr@reinblau.de>
2015-03-01 01:19:38 +0100

    [xdebug54]Removed crane file.

--- xdebug54/run.sh
+++ xdebug54/run.sh
@@ -1,26 +1,35 @@
-#!/usr/bin/env bash
-set -o errexit
+#!/bin/bash
 #set -x
 
 __DIR__="$(cd "$(dirname "${0}")"; echo $(pwd))"
 __BASE__="$(basename "${0}")"
 __FILE__="${__DIR__}/${__BASE__}"
+__P__="webgrind"
+
+
+build_project() {
+  if [ ! -f "/data/php54/conf/20-xdebug.ini" ];then
+    mkdir -p /data/php54/conf/
+    cp "${__DIR__}/20-xdebug.ini" /data/php54/conf/20-xdebug.ini
+  fi
+  if [[ ! -d "/data/www/${__P__}" ]];then
+    git clone --config core.filemode=false https://github.com/jokkedk/${__P__}.git /data/www/${__P__}/
+    chown -R vagrant: /data/www/${__P__}/
+  fi
+}
 
 start () {
-  if [ ! -f "$DATADIR/php/conf/20-xdebug.ini" ];then
-    mkdir -p $DATADIR/php/conf/
-    cp "${__DIR__}/20-xdebug.ini" "$DATADIR/php/conf/20-xdebug.ini"
-  fi
-  local project="webgrind"
-  if [[ ! -d "$DATADIR/www/$project/.git" ]];then
-    su vagrant -c "git clone --config core.filemode=false https://github.com/jokkedk/$project.git $DATADIR/www/$project/"
-    chown -R vagrant: $DATADIR/www/$project/
-  fi
-  cd "${__DIR__}" && crane lift
+  docker rm -f xdebug54 >/dev/null 2>&1
+  docker run --detach --env VIRTUAL_HOST=xdebug54.dev \
+    --env APACHE_USER_UID=1000 --hostname xdebug54.dev \
+    --link mysql:db --publish-all \
+    --volume /data/www/:/var/www/ \
+    --volume /data/php54/conf/20-xdebug.ini:/etc/php5/apache2/conf.d/20-xdebug.ini \
+    --name xdebug54 reinblau/xdebug54 >/dev/null
 }
 
 main () {
-  rbrequire --project=mysql
+  build_project
   start
 }
 
