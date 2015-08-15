5be3670aac10d9dbd6a659a1ec777a32c656ae1a
tobiasb <tobias.baehr@reinblau.de>
2015-08-15 17:53:46 +0200

    [mariadb]Added crane support. Override default my.cnf

--- mariadb/run.sh
+++ mariadb/run.sh
@@ -8,11 +8,7 @@ start () {
   if [ ! -d /data/mariadb/ ]; then
     mkdir -p /data/mariadb/10.0/data/
   fi
-  docker ps | grep "mariadb:10.0" >/dev/null || docker start mariadb_10_0 >/dev/null 2>&1 || \
-  docker run --detach --env MYSQL_ROOT_PASSWORD=root \
-    --publish-all \
-    --volume /data/mariadb/10.0/data/:/var/lib/mysql \
-    --name mariadb_10_0 mariadb:10.0 >/dev/null
+  cd $__DIR__ && crane run
 }
 
 main () {
