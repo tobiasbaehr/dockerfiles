1302e586a21a1053ad0981151a842e70c038e100
tobiasb <tobias.baehr@reinblau.de>
2016-09-04 16:48:04 +0200

    [cmd]Make image layer smaller. Updated drush. Removed yadd.

--- cmd/run.sh
+++ cmd/run.sh
@@ -9,7 +9,6 @@ docker run --hostname cmd.dev \
 --volume /data/www/:/var/www/ \
 --volume /data/user/.ssh/:/home/dev/.ssh/ \
 --volume /data/user/.gitconfig:/home/dev/.gitconfig \
---volume /data/user/yaddprojects:/home/dev/yaddprojects/ \
 --volume /data/user/.drush/:/home/dev/.drush/ \
 --volume /data/user/.gems/:/home/dev/.gems/ \
 --volume /data/user/.composer/:/home/dev/.composer/ \
