e5059d57764ee28b28b0bb8486822f73362ba1eb
tobiasb <tobias.baehr@reinblau.de>
2017-03-26 01:36:27 +0100

    [lamp:5.6]Change default remote host for xdebug.

--- lamp_56/xdebug_for_mac.sh
+++ lamp_56/xdebug_for_mac.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -x
 
-REMOTEHOST=${1:-10.200.10.1}
+REMOTEHOST=${1:-192.168.65.1}
 echo xdebug.remote_enable=On > /usr/local/etc/php/conf.d/docker-php-pecl-xdebug.ini
 echo xdebug.remote_host="$REMOTEHOST" >> /usr/local/etc/php/conf.d/docker-php-pecl-xdebug.ini
 service apache2 reload
