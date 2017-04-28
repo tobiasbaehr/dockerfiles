520865080828ae35b9402d6424081006c61da3cb
tobiasb <tobias.baehr@reinblau.de>
2017-04-28 11:17:33 +0200

    Updated update.sh script.

--- update.sh
+++ update.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
-#set -o errexit
+set -o errexit
 #set -x
 set -o nounset
-
-__DIR__="$(cd "$(dirname "${0}")"; echo $(pwd))"
-__BASE__="$(basename "${0}")"
-__FILE__="${__DIR__}/${__BASE__}"
-
-if [ "$(docker images |grep memcached)" ];then
-  docker pull memcached
-fi
 
 if [ "$(docker images |grep reinblau/solr3)" ];then
   docker pull reinblau/solr3
