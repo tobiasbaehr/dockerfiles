2fb90be419d38d2ac3eb5adf28a32283141e85c7
tobiasb <tobias.baehr@gzevd.de>
2018-01-16 11:37:36 +0100

    [rancher-cli] Added required dependency ca-certificates.

--- rancher-cli/build.sh
+++ rancher-cli/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
+#set -x
 docker build --rm -t gzevd/rancher-cli .
 
-docker run -it gzevd/rancher-cli:latest sh
+#docker run -it --env RANCHER_ACCESS_KEY=${RANCHER_ACCESS_KEY:-} --env RANCHER_SECRET_KEY=${RANCHER_SECRET_KEY:-} gzevd/rancher-cli:latest "$@"
