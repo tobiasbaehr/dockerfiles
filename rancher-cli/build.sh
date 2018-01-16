aaca5d6c7785640156b5840d543114ae580b76d8
tobiasb <tobias.baehr@gzevd.de>
2018-01-16 14:26:37 +0100

    Deleted dockerfile for rancher again.

--- rancher-cli/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-#set -x
-docker build --rm -t gzevd/rancher-cli .
-
-#docker run -it --env RANCHER_ACCESS_KEY=${RANCHER_ACCESS_KEY:-} --env RANCHER_SECRET_KEY=${RANCHER_SECRET_KEY:-} gzevd/rancher-cli:latest "$@"
